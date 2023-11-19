cbfcb0d45772ded8fcac1b64bb608931337f5f9f
marquee <84369120+savioalwd@users.noreply.github.com>
2023-11-19 23:42:11 +0530

    w

--- src/components/Pages/CreateAcct_2.jsx
+++ src/components/Pages/CreateAcct_2.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CreateAcct_2.scss";
 
 export default function CreateAcct_2()
 {
+    const [cardholdersname,setCardholdersname]=useState('');
+    const [cardno,setCardno]=useState('');
+    const [aadharno,setAadharno]=useState('')
+    const [phonenumber,setPhonenumber]=useState('')
+    const [otp,setOtp]=useState('');
     const handleProfile = () =>{
-        window.location.href="/profile"
+        if(cardholdersname.length===0)
+        {
+            alert("Enter cardholders name")
+        }
+        else if(cardno.length===0)
+        {
+            alert("Enter card number")
+        }
+        else if(aadharno.length===0)
+        {
+            alert("Enter aadhar number")
+        }
+        else if(phonenumber.length===0)
+        {
+            alert("Enter phone number")
+        }
+        else if(otp.length===0)
+        {
+            alert("Enter otp")
+        }
+        else
+        {
+            window.location.href="/profile"
+
+        }
     }
     return(
         <div className="create-2account">
@@ -15,25 +44,25 @@ export default function CreateAcct_2()
             <div className="create-2boxright">
                 <div className="create-2boxrightform">
                 <label className="username">Card Holders Name</label>
-                <input type="text" placeholder="Enter your name" className="hi"/>
+                <input type="text" placeholder="Enter your name" className="hi" onChange={(e)=>{setCardholdersname(e.target.value)}}/>
                 <div className="create-2boxcarddetails">
                     <div className="error">
                     <label>Card No</label>
-                <input type="text" placeholder="Enter Card Number"/>
+                <input type="text" placeholder="Enter Card Number"  onChange={(e)=>{setCardno(e.target.value)}}/>
                     </div>
                 <div className="error">
                 <label >Aadhar No</label>
-                <input type="text" placeholder="Enter Aadhar No"/>
+                <input type="text" placeholder="Enter Aadhar No"  onChange={(e)=>{setAadharno(e.target.value)}}/>
                 </div>
                 </div>
                 <div className="create-2boxcarddetails">
                     <div className="error">
                     <label>Phone Number</label>
-                <input type="text" placeholder="Enter Phone Number"/>
+                <input type="text" placeholder="Enter Phone Number"  onChange={(e)=>{setPhonenumber(e.target.value)}}/>
                     </div>
                 <div className="error">
                 <label >One Time Password</label>
-                <input type="text" placeholder="Enter OTP"/>
+                <input type="text" placeholder="Enter OTP"  onChange={(e)=>{setOtp(e.target.value)}}/>
                 </div>
                 </div>
                 </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import './navbar.scss';
 
 const navbar = () =>{
+    const handleLogout = () =>{
+        window.location.href="/"
+    }
     return(
         <div className="navbar">
             <div className="title">
                 E-RATION
             </div>
-            <div className="login">
+            <div className="login" onClick={()=>{handleLogout()}}>
                 LOGOUT
             </div>
         </div>
