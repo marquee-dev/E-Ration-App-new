ba7e805177967eb3162a6159344bdf0ea0604964
arkhmknight <sreenadhmanmadhan@gmail.com>
2023-12-10 18:48:21 +0530

    backend

--- src/components/Pages/AddTransaction.jsx
+++ src/components/Pages/AddTransaction.jsx
@@ -4,9 +4,10 @@ import NavBar from '../navbar.jsx';
 import { useNavigate } from "react-router-dom";
 import Modal from "react-modal";
 import CloseIcon from '@mui/icons-material/Close';
-
+import axios from "axios";
 const AddTransaction = () => {
   const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleDetails =() => {
     navigate("/details");
@@ -16,6 +17,14 @@ const handleStocks = () =>{
   navigate("/stocks");
 }
 const openModal = () => {
+  axios.get("http://localhost:4000/viewappointments")
+  .then(function (response) {
+    console.log('Axios request successful');
+    setUserData(response.data);
+  })
+  .catch(function (error) {
+    console.error('Error fetching data:', error);
+  });
   setIsModalOpen(true);
 };
 
@@ -39,6 +48,24 @@ const modalContent = (
             <div className="rel2-s">
               <div className="line-s"></div>
             </div>
+            <div className="rel3-s">
+            <div className="id-s">
+            {userData?.map(user => (
+  <span key={user.idbooking}>{user.idbooking}</span>
+))}
+            </div>
+            <div className="username-s">
+            {userData?.map(user => (
+  <span key={user.username}>{user.username}</span>
+))}
+            </div>
+            <div className="date-s">
+            {userData?.map(user => (
+  <span key={user.date}>{user.date}</span>
+))}
+            </div>
+            </div>
+            
           </div>
   </div>
   
--- src/components/Pages/Details.jsx
+++ src/components/Pages/Details.jsx
@@ -33,6 +33,14 @@ const Details = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
+    axios.get("http://localhost:4000/viewappointments")
+    .then(function (response) {
+      console.log('Axios request successful');
+      setUserData(response.data);
+    })
+    .catch(function (error) {
+      console.error('Error fetching data:', error);
+    });
     setIsModalOpen(true);
   };
 
@@ -56,10 +64,29 @@ const Details = () => {
               <div className="rel2-s">
                 <div className="line-s"></div>
               </div>
+              <div className="rel3-s">
+              <div className="id-s">
+              {userData?.map(user => (
+    <span key={user.idbooking}>{user.idbooking}</span>
+  ))}
+              </div>
+              <div className="username-s">
+              {userData?.map(user => (
+    <span key={user.username}>{user.username}</span>
+  ))}
+              </div>
+              <div className="date-s">
+              {userData?.map(user => (
+    <span key={user.date}>{user.date}</span>
+  ))}
+              </div>
+              </div>
+              
             </div>
     </div>
     
   );
+
 
   return (
     <div className="page-d">
