5a0329dff73c2a7598315de013faeb9b5d92d873
marquee <84369120+savioalwd@users.noreply.github.com>
2023-12-10 18:27:50 +0530

    aa

--- src/components/Pages/Stocks.jsx
+++ src/components/Pages/Stocks.jsx
@@ -2,15 +2,25 @@
 import React, { useState } from "react";
 import "./Stocks.scss";
 import Modal from "react-modal";
+import axios from "axios";
 import NavBar from "../navbar.jsx";
 import { useNavigate } from "react-router-dom";
 import CloseIcon from '@mui/icons-material/Close';
 
 const Stocks = () => {
   const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
+    axios.get("http://localhost:4000/viewappointments")
+    .then(function (response) {
+      console.log('Axios request successful');
+      setUserData(response.data);
+    })
+    .catch(function (error) {
+      console.error('Error fetching data:', error);
+    });
     setIsModalOpen(true);
   };
 
@@ -25,7 +35,7 @@ const Stocks = () => {
   const handleReport = () => {
     navigate("/addtransaction");
   };
-
+  
   const modalContent = (
     <div className="modalcontent">
     <div className="appointment">
@@ -43,6 +53,24 @@ const Stocks = () => {
               <div className="rel2-s">
                 <div className="line-s"></div>
               </div>
+              <div className="rel3-s">
+              <div className="id-s">
+              {userData?.map(user => (
+    <span key={user.idbooking}>{user.idbooking}</span>
+  ))}
+              </div>
+              <div className="username-s">
+              {userData?.map(user => (
+    <span key={user.username}>{user.username}</span>
+  ))}
+              </div>
+              <div className="date-s">
+              {userData?.map(user => (
+    <span key={user.date}>{user.date}</span>
+  ))}
+              </div>
+              </div>
+              
             </div>
     </div>
     
